4608d58da5eae9113cd9e687a4b37bb2051d21e7
Your Name <you@example.com>
2022-02-20 14:49:27 -0700

    update css, set mobile-shrink property

--- src/components/section/sub/doge-dash.js
+++ src/components/section/sub/doge-dash.js
@@ -6,8 +6,10 @@ export function DogeDash() {
                   <div className='bottom-space-sm' />
 
           <div className='row'>
-          <iframe src="https://i.simmer.io/@757372one/doge-1"  
-          style={{"width":"960px", "height":"600px", "border":"0"}}></iframe>
+            <div className='mobile-shrink-2'>
+            <iframe src="https://i.simmer.io/@757372one/doge-1"  title='doge-dash'
+            style={{"width":"960px", "height":"600px", "border":"0"}}></iframe>
+          </div>
           </div>
       </div>
   );
