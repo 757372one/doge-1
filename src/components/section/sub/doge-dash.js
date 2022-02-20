f1ffae41f5bd16a171605b51c71a83a5b2902a92
Your Name <you@example.com>
2022-02-20 08:54:49 -0700

    update doge-1 game link

--- src/components/section/sub/doge-dash.js
+++ src/components/section/sub/doge-dash.js
@@ -6,7 +6,7 @@ export function DogeDash() {
                   <div className='bottom-space-sm' />
 
           <div className='row'>
-          <iframe src="https://i.simmer.io/@WhizGaming/doge1dash" 
+          <iframe src="https://i.simmer.io/@757372one/doge-1"  
           style={{"width":"960px", "height":"600px", "border":"0"}}></iframe>
           </div>
       </div>
